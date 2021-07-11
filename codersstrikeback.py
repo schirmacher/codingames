3afa3dd2321c5e796fbb88c373d286dd545dac9f
schirmacher <arne@schirmacher.de>
2021-07-11 12:20:21 +0200

    improvement suggested by tutorial
    rank is about 14000 / 50000

--- codersstrikeback.py
+++ codersstrikeback.py
@@ -14,6 +14,11 @@ while True:
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
 
+    if next_checkpoint_angle > 90 or next_checkpoint_angle < -90:
+        thrust = 0
+    else:
+        thrust = 100
+
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
